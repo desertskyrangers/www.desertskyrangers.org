c3374d17ad43b7e3f7f3bdd19838bff17b010e45
Mark Soderquist <mark@soderquist.net>
2021-08-28 11:09:52 -0600

    More content

--- src/Home.js
+++ src/Home.js
@@ -26,16 +26,17 @@ export default class Home extends React.Component {
 							station</a> nearby that gives realtime flight conditions for the
 							area.
 						</p>
-						<h5><a href="/"><FontAwesomeIcon icon={faMapMarkerAlt}/></a> Pony Express Elementary</h5>
+						<h5><a href="https://goo.gl/maps/oLuZKi1qZzowQPj58"><FontAwesomeIcon icon={faMapMarkerAlt}/></a> Pony Express Elementary</h5>
 						<p>
 							A large rectangular field with only a few trees next to the school. This
 							field provides a long and wide field perfect for beginner pilots and
 							experts alike. Due to the open space to the northeast there are no houses
 							in the immediate area. However, please do not fly during school hours.
 						</p>
-						<h5><a href="https://slco.org/parks/wardle-fields-regional-park/"><FontAwesomeIcon icon={faMapMarkerAlt}/></a> Wardle Fields Regional Park</h5>
+						<h5><a href="https://goo.gl/maps/WfuVVZqe4Qv32PX27"><FontAwesomeIcon icon={faMapMarkerAlt}/></a> Wardle Fields Regional Park</h5>
 						<p>
-							Large open fields including a large, bowl shaped field not marked for
+							<a href='https://slco.org/parks/wardle-fields-regional-park/'>Wardle Fields Regional Park</a> has
+							large open fields including a large, bowl shaped field not marked for
 							sports. There are only a few small trees near the field and a gently
 							sloping hill on the edge of the bowl.
 						</p>
