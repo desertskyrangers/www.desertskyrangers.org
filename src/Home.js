94168224a857af5a606f826a2a913636c066b342
msoderquist <msoderquist@mastercontrol.com>
2021-10-05 12:57:13 -0600

    Updated location info

--- src/Home.js
+++ src/Home.js
@@ -28,22 +28,23 @@ export default class Home extends React.Component {
 							If you're wondering what's up in the sky, it might just be us hanging
 							out at some of our favorite locations:
 						</p>
-						<h5><a href="https://goo.gl/maps/i3NkAMGppHkqknxw6"><FontAwesomeIcon icon={faMapMarkerAlt}/></a> Morning Cloak Park</h5>
+						<h5><a href="https://goo.gl/maps/i3NkAMGppHkqknxw6"><FontAwesomeIcon icon={faMapMarkerAlt}/></a> Morning Cloak Park (Thursdays before sunset)</h5>
 						<p>
-							A small, bowl shaped park surrounded by small trees. This park is
-							popular with the group due to the proximity of several members.
+							A small, bowl shaped park surrounded by small trees. This location
+							is a bit challenging due to the small field and surrounding trees
+							but is popular with the group due to the proximity of several members.
 							Mark has a <a href="http://mark.soderquist.net/weather/">weather
 							station</a> nearby that gives realtime flight conditions for the
 							area.
 						</p>
-						<h5><a href="https://goo.gl/maps/oLuZKi1qZzowQPj58"><FontAwesomeIcon icon={faMapMarkerAlt}/></a> Pony Express Elementary</h5>
+						<h5><a href="https://goo.gl/maps/oLuZKi1qZzowQPj58"><FontAwesomeIcon icon={faMapMarkerAlt}/></a> Pony Express Elementary (some Saturdays)</h5>
 						<p>
 							A large rectangular field with only a few trees next to the school. This
 							field provides a long and wide field perfect for beginner pilots and
 							experts alike. Due to the open space to the northeast there are no houses
 							in the immediate area. However, please do not fly during school hours.
 						</p>
-						<h5><a href="https://goo.gl/maps/WfuVVZqe4Qv32PX27"><FontAwesomeIcon icon={faMapMarkerAlt}/></a> Wardle Fields Regional Park</h5>
+						<h5><a href="https://goo.gl/maps/WfuVVZqe4Qv32PX27"><FontAwesomeIcon icon={faMapMarkerAlt}/></a> Wardle Fields Regional Park (some Saturdays)</h5>
 						<p>
 							<a href='https://slco.org/parks/wardle-fields-regional-park/'>Wardle Fields Regional Park</a> has
 							large open fields including a large, bowl shaped field not marked for
