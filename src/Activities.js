62f4b450e09a792d7c62456782deb782b51fd5ee
msoderquist <msoderquist@mastercontrol.com>
2021-11-22 11:39:09 -0700

    Photo captions

--- src/Activities.js
+++ src/Activities.js
@@ -7,7 +7,10 @@ export default class Activities extends React.Component {
 			<div className='content'>
 
 				<h2>Nov 2021 Fun-Fly</h2>
-				<img className='photo' src='/photos/dsr-funfly-2021-11-20.jpg' alt='DRS Fun Fly'/>
+				<figure>
+					<img className='photo' src='/photos/dsr-funfly-2021-11-20.jpg' alt='DRS Fun Fly'/>
+					<figcaption>Hudson at the controls, Mark and Percy(dog) looking on.</figcaption>
+				</figure>
 				<p>
 					We held an impromptu fun fly on Saturday, Nov 20th. For the first time we had all members
 					of the club available for <a href='/about'>group photo</a> and had a chilly morning of
--- src/Home.js
+++ src/Home.js
@@ -14,12 +14,15 @@ export default class Home extends React.Component {
                     <div className="column">
 
                         <h2>Nov 2021 Fun-Fly</h2>
-                        <img className='photo' src='/photos/dsr-funfly-2021-11-20.jpg' alt='DRS Fun Fly'/>
+                        <figure>
+                            <img className='photo' src='/photos/dsr-funfly-2021-11-20.jpg' alt='DRS Fun Fly'/>
+                            <figcaption>Hudson at the controls, Mark and Percy(dog) looking on.</figcaption>
+                        </figure>
                         <p>
                             We held an impromptu fun fly on Saturday, Nov 20th. For the first time we had all members
                             of the club available for <a href='/about'>group photo</a> and had a chilly morning of
-							flying. Despite the cool weather we had a great time getting together, increasing our flight
-							times and sharing stories.
+                            flying. Despite the cool weather we had a great time getting together, increasing our flight
+                            times and sharing stories.
                         </p>
 
                         <h2>Where We Fly</h2>
