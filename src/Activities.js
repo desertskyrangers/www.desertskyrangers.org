26fa074202daaf6bd838b49cabc3125005f87b16
msoderquist <msoderquist@mastercontrol.com>
2021-11-29 19:46:54 -0700

    Change Fun Fly to Ranger Rally

--- src/Activities.js
+++ src/Activities.js
@@ -6,13 +6,13 @@ export default class Activities extends React.Component {
 		return (
 			<div className='content'>
 
-				<h2>Nov 2021 Fun-Fly</h2>
+				<h2>Nov 2021 Ranger Rally</h2>
 				<figure>
-					<img className='photo' src='/photos/dsr-funfly-2021-11-20.jpg' alt='DRS Fun Fly'/>
+					<img className='photo' src='/photos/dsr-ranger-rally-2021-11-20.jpg' alt='DRS Ranger Rally'/>
 					<figcaption>Hudson at the controls, Mark and Percy looking on.</figcaption>
 				</figure>
 				<p>
-					We held an impromptu fun fly on Saturday, Nov 20th. For the first time we had all members
+					We held an impromptu Ranger Rally on Saturday, Nov 20th. For the first time we had all members
 					of the club available for <a href='/about'>group photo</a> and had a chilly morning of
 					flying. Despite the cool weather we had a great time getting together, increasing our flight
 					times and sharing stories.
--- src/Home.js
+++ src/Home.js
@@ -15,11 +15,11 @@ export default class Home extends React.Component {
 
                         <h2>Nov 2021 Fun-Fly</h2>
                         <figure>
-                            <img className='photo' src='/photos/dsr-funfly-2021-11-20.jpg' alt='DRS Fun Fly'/>
+                            <img className='photo' src='/photos/dsr-ranger-rally-2021-11-20.jpg' alt='DRS Ranger Rally'/>
                             <figcaption>Hudson at the controls, Mark and Percy looking on.</figcaption>
                         </figure>
                         <p>
-                            We held an impromptu fun fly on Saturday, Nov 20th. For the first time we had all members
+                            We held an impromptu Ranger Rally on Saturday, Nov 20th. For the first time we had all members
                             of the club available for <a href='/about'>group photo</a> and had a chilly morning of
                             flying. Despite the cool weather we had a great time getting together, increasing our flight
                             times and sharing stories.
