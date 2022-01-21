cb0ebc99e562cf942e1b14a8692512e2320136b4
msoderquist <msoderquist@mastercontrol.com>
2022-01-21 09:03:02 -0700

    Change FlightDeck page to be more mobile friendly

--- src/FlightDeck.js
+++ src/FlightDeck.js
@@ -5,22 +5,23 @@ export default function FlightDeck(props) {
 	return (
 		<div className='content'>
 
-			<div className='banner'>
+			<a href='https://flightdeck.desertskyrangers.org/login' className='banner'>
 				<img width='64' height='64' src='https://flightdeck.desertskyrangers.org/logo.png' alt="DSR"/>
 				<h1>FlightDeck</h1>
-				<div className='gap-2'/>
-				<a href='https://flightdeck.desertskyrangers.org/register'>
-					<button className='action-button'>Create an Account</button>
-				</a>
-				<a href='https://flightdeck.desertskyrangers.org/login'>
-					<button className='action-button'>Log In</button>
-				</a>
-			</div>
+			</a>
 
 			<p>
 				Welcome to the FlightDeck, your personal destination for UAV management,
 				flight logging and pilot data. Built on modern cloud and web technology,
-				FlightDeck is your one stop for all your UAV pilot needs. Features include:
+				FlightDeck is your one stop for all your UAV pilot needs.
+			</p>
+
+			<a href='https://flightdeck.desertskyrangers.org/register'>
+				<button className='action-button'>Create an Account</button>
+			</a>
+
+			<p>
+				Features include:
 			</p>
 			<ul>
 				<li>Aircraft asset management</li>
