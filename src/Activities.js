031b39bbf7f0520859f686061791d9ccc0358003
msoderquist <msoderquist@mastercontrol.com>
2022-02-23 13:27:55 -0700

    Jan Ranger Rally

--- src/Activities.js
+++ src/Activities.js
@@ -46,16 +46,18 @@ export default class Activities extends React.Component {
 				</p>
 
 				<h2>{Icons.RALLY} Nov 2021 Ranger Rally</h2>
-				<figure>
-					<img className='photo' src='/photos/dsr-ranger-rally-2021-11-20.jpg' alt='DRS Ranger Rally'/>
-					<figcaption>Hudson at the controls, Mark and Percy looking on.</figcaption>
-				</figure>
 				<p>
 					We held an impromptu Ranger Rally on Saturday, Nov 20th. For the first time we had all members
 					of the club available for <a href='/about'>group photo</a> and had a chilly morning of
 					flying. Despite the cool weather we had a great time getting together, increasing our flight
 					times and sharing stories.
 				</p>
+				<div className='hbox-wrappable'>
+					<figure>
+						<img className='photo' src='/photos/dsr-ranger-rally-2021-11-20.jpg' alt='DRS Ranger Rally'/>
+						<figcaption>Hudson at the controls, Mark and Percy looking on.</figcaption>
+					</figure>
+				</div>
 
 			</div>
 		)
