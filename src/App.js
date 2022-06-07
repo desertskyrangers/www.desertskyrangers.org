6b860c7a82ccab61ceedff2b2a19bcc1a0d8bf06
msoderquist <msoderquist@mastercontrol.com>
2022-06-06 19:34:46 -0600

    Fix navigation with trailing slash

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import {BrowserRouter as Router, Navigate, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from "./Header";
 import Home from "./Home";
 import About from "./About";
