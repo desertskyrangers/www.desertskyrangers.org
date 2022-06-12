d318fc8343f4f995006af45fecd879b415ef4e7f
Mark Soderquist <mark@soderquist.net>
2022-06-12 15:05:44 -0600

    Add DSR-001 3D parts link

--- src/Plans.js
+++ src/Plans.js
@@ -13,7 +13,7 @@ export default function Plans(props) {
 				<h2 id="dsr-001"><img className='icon' src='/plans/dsr-001/dsr-001-v1-photo.jpeg' alt="DSR-001"/> DSR-001 Aquila</h2>
 				<a href="/plans/dsr-001/dsr-001-v1-overview.pdf">Aircraft Overview</a><br/>
 				<a href="/plans/dsr-001/dsr-001-v1-fuselage.pdf">Fuselage Plans</a><br/>
-				<a href="/plans/dsr-001/dsr-001-v1-wing.pdf">Wing Plans</a>
+				<a href="/plans/dsr-001/dsr-001-v1-wing.pdf">Wing Plans</a><br/>
 				<a href="https://www.thingiverse.com/thing:5409435">3D Parts</a>
 				<div className='hbox-wrappable'>
 					<figure>
