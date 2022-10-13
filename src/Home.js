8bb1eeed157ed43ed2585243d89f91de8ca6d74f
msoderquist <msoderquist@mastercontrol.com>
2022-10-12 19:17:22 -0600

    Add support and twitter

--- src/Home.js
+++ src/Home.js
@@ -11,8 +11,8 @@ export default class Home extends React.Component {
 				<div className="row">
 					<div className="info column">
 						<Link to='/tools/flightdeck' className="infoitem">{Icons.FLIGHTDECK}FlightDeck</Link>
-						<Link to="/safety" className="infoitem">{Icons.SAFETY} Safety Rules</Link>
 						<Link to="/locations" className="infoitem">{Icons.LOCATION} Where We Fly</Link>
+						<Link to="/safety" className="infoitem">{Icons.SAFETY} Event Rules</Link>
 						<Link to='/support' className='infoitem'>{Icons.HANDSHAKE} How to Support Us</Link>
 						<a href="https://twitter.com/desertskyranger?ref_src=twsrc%5Etfw" className="infoitem">{Icons.TWITTER} Follow Us on Twitter</a>
 					</div>
