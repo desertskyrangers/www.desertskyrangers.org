49f72770494cdd281a555789dc87dd2391e8c264
msoderquist <msoderquist@mastercontrol.com>
2024-02-15 18:20:42 -0700

    Update events

--- src/Events.js
+++ src/Events.js
@@ -7,19 +7,62 @@ export default class Events extends React.Component {
 			<div className='content'>
 				<h1>Events</h1>
 
-				<h2>2024</h2>
-				<h6>Jan 20 @ 10:00am - Ranger Rendezvous</h6>
+				<h2 className='infoitem'>2024</h2>
 				<h5>Feb 17 @ 09:00am - Ranger Rally</h5>
+				<p className='content'>
+					Wardle Fields Regional Park<br/>
+					14148 S 2700 W, Bluffdale, UT 84065
+				</p>
 				<h6>Mar 16 @ 10:00am - Ranger Rendezvous</h6>
+				<p className='content'>
+					Soderquist Residence<br/>
+					13626 Bluewing Way, Riverton, UT 84096
+				</p>
 				<h6>Apr 20 @ 10:00am - Ranger Rendezvous</h6>
+				<p className='content'>
+					Soderquist Residence<br/>
+					13626 Bluewing Way, Riverton, UT 84096
+				</p>
 				<h5>May 18 @ 09:00am - Ranger Rally</h5>
+				<p className='content'>
+					Wardle Fields Regional Park<br/>
+					14148 S 2700 W, Bluffdale, UT 84065
+				</p>
 				<h6>Jun 15 @ 10:00am - Ranger Rendezvous</h6>
+				<p className='content'>
+					Soderquist Residence<br/>
+					13626 Bluewing Way, Riverton, UT 84096
+				</p>
 				<h6>Jul 20 @ 10:00am - Ranger Rendezvous</h6>
+				<p className='content'>
+					Soderquist Residence<br/>
+					13626 Bluewing Way, Riverton, UT 84096
+				</p>
 				<h5>Aug 17 @ 09:00am - Ranger Rally</h5>
+				<p className='content'>
+					Wardle Fields Regional Park<br/>
+					14148 S 2700 W, Bluffdale, UT 84065
+				</p>
 				<h6>Sep 21 @ 10:00am - Ranger Rendezvous</h6>
+				<p className='content'>
+					Soderquist Residence<br/>
+					13626 Bluewing Way, Riverton, UT 84096
+				</p>
 				<h5>Oct 19 @ 09:00am - Ranger Rally</h5>
+				<p className='content'>
+					Wardle Fields Regional Park<br/>
+					14148 S 2700 W, Bluffdale, UT 84065
+				</p>
 				<h6>Nov 16 @ 10:00am - Ranger Rendezvous</h6>
+				<p className='content'>
+					Soderquist Residence<br/>
+					13626 Bluewing Way, Riverton, UT 84096
+				</p>
 				<h6>Dec 14 @ 10:00am - Ranger Rendezvous</h6>
+				<p className='content'>
+					Soderquist Residence<br/>
+					13626 Bluewing Way, Riverton, UT 84096
+				</p>
 			</div>
 		)
 	}
